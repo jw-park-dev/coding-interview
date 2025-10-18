a4645dacacc32ab77ee7f1ec56e346dd59e3d9d9
jw-park-dev <jinwoopark.dev@gmail.com>
2025-10-18 14:28:46 +0900

    Create n의-배수.java

---
+++ Programmers/Math/n의-배수.java
@@ -0,0 +1,15 @@
+/*
+ * [문제 링크]
+ * https://school.programmers.co.kr/learn/courses/30/lessons/181937
+ * [비고]
+ * - x가 y의 배수가 되기 위한 조건: x를 y로 나눴을 때의 나머지가 0.
+ */
+
+class Solution {
+    public int solution(int num, int n) {
+        if (num % n == 0) {
+            return 1;
+        }
+        return 0;
+    }
+}
