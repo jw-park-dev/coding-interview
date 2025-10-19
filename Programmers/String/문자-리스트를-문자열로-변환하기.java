1a63965220e2933b57b27415ba30d70eee8b9384
jw-park-dev <jinwoopark.dev@gmail.com>
2025-10-19 13:13:52 +0900

    Create 문자-리스트를-문자열로-변환하기.java

---
+++ Programmers/String/문자-리스트를-문자열로-변환하기.java
@@ -0,0 +1,14 @@
+/*
+ * [문제 링크]
+ * https://school.programmers.co.kr/learn/courses/30/lessons/181941
+ */
+
+class Solution {
+    public String solution(String[] arr) {
+        StringBuilder sb = new StringBuilder();
+        for (String str : arr) {
+            sb.append(str);
+        }
+        return sb.toString();
+    }
+}
