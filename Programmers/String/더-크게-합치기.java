2df834b908abbe79557a3fcc969aa7f72c6c063d
jw-park-dev <jinwoopark.dev@gmail.com>
2025-10-19 15:13:28 +0900

    Update 더-크게-합치기.java

--- Programmers/String/더-크게-합치기.java
+++ Programmers/String/더-크게-합치기.java
@@ -9,7 +9,6 @@
 
 class Solution {
     public int solution(int a, int b) {
-        int answer = 0;
         int aThenB = Integer.parseInt(a + "" + b);
         int bThenA = Integer.parseInt(b + "" + a);
         
