decd0212ce7a02b892bce4b1c0ef64d8142d8441
jw-park-dev <jinwoopark.dev@gmail.com>
2025-10-21 22:15:01 +0900

    Create 정수-부분.java

---
+++ Programmers/Math/정수-부분.java
@@ -0,0 +1,12 @@
+/*
+ * [문제 링크]
+ * https://school.programmers.co.kr/learn/courses/30/lessons/181850?language=java
+ * [비고]
+ * double형 실수를 (int)로 casting하면 double의 정수 부분을 반환함.
+ */
+
+class Solution {
+    public int solution(double flo) {
+        return (int)flo;
+    }
+}
