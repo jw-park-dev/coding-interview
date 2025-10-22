45c382ae1e178a42551bb27d1fe1c04a6b008129
jw-park-dev <jinwoopark.dev@gmail.com>
2025-10-22 21:47:01 +0900

    Create 정수-찾기.java

---
+++ Programmers/Math/정수-찾기.java
@@ -0,0 +1,15 @@
+/*
+ * [문제 링크]
+ * https://school.programmers.co.kr/learn/courses/30/lessons/181840?language=java
+ */
+
+class Solution {
+    public int solution(int[] num_list, int n) {
+        for (int num : num_list) {
+            if (num == n) {
+                return 1;
+            }
+        }
+        return 0;
+    }
+}
