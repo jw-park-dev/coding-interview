b391ae5e2db1f17112779ae09ab0db3940822c07
jw-park-dev <jinwoopark.dev@gmail.com>
2025-10-23 20:53:58 +0900

    Create 문자열로-변환.java

---
+++ Programmers/String/문자열로-변환.java
@@ -0,0 +1,14 @@
+/*
+ * [문제 링크]
+ * https://school.programmers.co.kr/learn/courses/30/lessons/181845?language=java
+ * [비고]
+ * int형을 String으로 변환하는 다양한 방법 익혀둘 것.
+ */
+
+class Solution {
+    public String solution(int n) {
+        // return Integer.toString(n)
+        // return String.valueOf(n)
+        return "" + n;
+    }
+}
