8dcd5816fe62c8ac4c68ab2fe61eadf69f820a1c
jw-park-dev <jinwoopark.dev@gmail.com>
2025-10-25 21:27:26 +0900

    Create 문자열을-정수로-변환하기.java

---
+++ Programmers/String/문자열을-정수로-변환하기.java
@@ -0,0 +1,10 @@
+/*
+ * [문제 링크]
+ * https://school.programmers.co.kr/learn/courses/30/lessons/181848?language=java
+ */
+
+class Solution {
+    public int solution(String n_str) {
+        return Integer.parseInt(n_str);
+    }
+}
