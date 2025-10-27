6dc49f8d532f31cc9dc2e3a96cd771296624464c
jw-park-dev <jinwoopark.dev@gmail.com>
2025-10-27 20:51:54 +0900

    Create 홀수-vs-짝수.java

---
+++ Programmers/Array/홀수-vs-짝수.java
@@ -0,0 +1,21 @@
+/*
+ * [문제 링크]
+ * https://school.programmers.co.kr/learn/courses/30/lessons/181887
+ */
+
+class Solution {
+    public int solution(int[] num_list) {
+        int oddSum = 0;
+        int evenSum = 0;
+        for (int i = 0; i < num_list.length; i++) {
+            if (i % 2 == 0) {
+                evenSum += num_list[i];
+            }
+            else {
+                oddSum += num_list[i];
+            }
+        }
+        
+        return oddSum >= evenSum ? oddSum : evenSum;
+    }
+}
